582bc3ee8e234b48e6bdfb8a880a99b090b37420
tituswe <tituslowesg@gmail.com>
2023-09-10 23:28:27 +0800

    Assignment 1

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,8 @@ import QuestionModal from './components/Question/QuestionModal.tsx';
 import './index.css';
 import { store } from './store.ts';
 
+// Assignment 1 complete
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
