c203a3d29b6c16cf66f8646313ecc3168b6fb4de
garethkoh <e0533656@u.nus.edu>
2023-09-18 02:56:24 +0800

    Add routing configuration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import QuestionDetailsModal from './components/Question/QuestionDetailsModal.tsx';
 import QuestionModal from './components/Question/QuestionModal.tsx';
+import {UserDashboard} from "./pages/UserDashboard";
 import './index.css';
 import { store } from './store.ts';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path:"/",
+		element: <App/>,
+	},
+	{
+		path:"/userDashboard/:username",
+		element: <UserDashboard/>,
+	}
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
 			<QuestionModal />
 			<QuestionDetailsModal />
+			<RouterProvider router={router} />
 		</Provider>
 	</React.StrictMode>
 );
