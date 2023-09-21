a5cbce1eb8969848daff205abc5ddbf80314a45e
garethkoh <e0533656@u.nus.edu>
2023-09-22 02:50:15 +0800

    Remove Question table from landing page for assignment 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import QuestionTable from './components/Question/QuestionTable';
 import {Login} from "./pages/Login";
 import {Register} from "./pages/Register";
 import {useState} from "react";
@@ -14,9 +13,9 @@ function App() {
 	return (
 		<main className="p-24">
 			<h1 className="text-3xl font-bold">G19 Assessment</h1>
-			<section className="p-8">
-				<QuestionTable />
-			</section>
+			{/*<section className="p-8">*/}
+			{/*	<QuestionTable />*/}
+			{/*</section>*/}
 			{
 				currentForm === 'login' ? <Login onFormSwitch={toggleForm}/> : <Register onFormSwitch={toggleForm}/>
 			}
