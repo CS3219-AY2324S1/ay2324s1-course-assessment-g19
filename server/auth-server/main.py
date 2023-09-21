b3e3a3840377d42d98eae5ba80b61ba1289a2af3
garethkoh <e0533656@u.nus.edu>
2023-09-22 02:51:10 +0800

    Add change password functionality

--- server/auth-server/main.py
+++ server/auth-server/main.py
@@ -6,7 +6,10 @@ from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
-
+from sqlalchemy.exc import IntegrityError
+from fastapi.responses import JSONResponse
+import bcrypt
+from passlib.context import CryptContext
 
 origins = [
     "http://localhost:5173",
@@ -31,6 +34,10 @@ class UserBase(BaseModel):
     password: str
 
 
+class VerifyPasswordRequest(BaseModel):
+    password: str
+
+
 def get_db():
     db = SessionLocal()
     try:
@@ -46,8 +53,9 @@ db_dependency = Annotated[Session, Depends(get_db)]
 async def get_user(username: str, db: db_dependency):
     result = db.query(models.Users).filter(models.Users.username == username).first()
     if not result:
-        raise HTTPException(status_code=404, detail='no such user')
+        return JSONResponse(status_code=404, content={"error": "No such user"})
     return result
+
 
 @app.post("/users/")
 async def create_user(user: UserBase, db: db_dependency):
@@ -56,6 +64,7 @@ async def create_user(user: UserBase, db: db_dependency):
     db.commit()
     db.refresh(db_user)
     return db_user
+
 
 @app.delete("/users/{username}")
 async def delete_user(username: str, db: db_dependency):
@@ -66,12 +75,43 @@ async def delete_user(username: str, db: db_dependency):
     db.commit()
     return {"message": f"User {username} has been deleted"}
 
+
 @app.put("/change-user/{username}/{new_username}")
 async def update_user(username: str, new_username: str, db: db_dependency):
     user = db.query(models.Users).filter(models.Users.username == username).first()
+    existing_user = db.query(models.Users).filter(models.Users.username == new_username).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail='Username already in use')
     if not user:
         raise HTTPException(status_code=404, detail='no such user')
     user.username = new_username
-    db.commit()
-    return {"message": f"Username updated to {new_username}"}
+    try:
+        db.commit()
+        return {"message": f"Username updated to {new_username}"}
+    except IntegrityError as e:
+        db.rollback()
+        return JSONResponse(status_code=400, content={"error": "Username clash"})
 
+
+@app.post("/verify-password/{username}")
+def verify_user_password(username: str, request_data: VerifyPasswordRequest, db: db_dependency):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    if not bcrypt.checkpw(request_data.password.encode('utf-8'), user.password.encode('utf-8')):
+        raise HTTPException(status_code=400, detail="Incorrect old password")
+    return {"message": "Password verified successfully"}
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+@app.put("/change-password/{username}")
+def change_user_password(username:str, request_data: VerifyPasswordRequest, db: db_dependency):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail='no such user')
+    hashed_password = pwd_context.hash(request_data.password)
+    user.password = hashed_password
+    db.commit()
+    return {"message": "Password changed successfully"}
