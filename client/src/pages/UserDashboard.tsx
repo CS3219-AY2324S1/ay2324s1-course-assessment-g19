a5a107b56957c136c27d26187ae782416e98b8bb
garethkoh <e0533656@u.nus.edu>
2023-09-22 02:53:41 +0800

    Reorganise dashboard page to show relevant info

--- client/src/pages/UserDashboard.tsx
+++ client/src/pages/UserDashboard.tsx
@@ -1,67 +1,35 @@
-import { useState} from "react";
-import {useNavigate, useParams} from "react-router-dom";
-import api from "./reactapi";
+import { useParams} from "react-router-dom";
+import { Link } from 'react-router-dom';
+import QuestionTable from '../components/Question/QuestionTable';
 
 
 export const UserDashboard = () => {
-    const [new_username, setNewUser] = useState('');
-    const [new_password, setNewPass] = useState('');
     const {username} = useParams();
-    const [currentUsername, setCurrentUsername] = useState(username);
 
-    const deleteAccount = async () => {
-        await api.delete(`/users/${username}`);
-        console.log("deleted:", {username});
-    }
-
-    const navigate = useNavigate();
-
-    const changeUsername = async (e) => {
-        e.preventDefault()
-        await api.put(`/change-user/${currentUsername}/${new_username}`);
-        setCurrentUsername(new_username);
-        console.log("username updated from " + username + " to " + new_username);
-        navigate(`/userDashboard/${new_username}`);
-    }
-    const changePassword = async () => {}
 
     return (
-        <div>
-            <h1>Welcome to your Dashboard, {currentUsername}</h1>
+        <div className="flex flex-col justify-center items-center">
+            <div className="flex justify-between w-full px-4 mt-4">
+                <h1 className="font-bold text-2xl">Welcome, {username}</h1>
 
-            <form onSubmit={changeUsername}>
-                <h1>Change Username</h1>
-                <input value={new_username} onChange={(e) => setNewUser(e.target.value)} type="text" placeholder="New Username" required/>
-                <button type="submit">Confirm change</button>
-            </form>
+                <Link className="hover:text-blue-600" to={`/accountSettings/${username}`}>
+                    Account Settings
+                </Link>
+                <Link className="hover:text-blue-600" to={`/`}>
+                    Log out
+                </Link>
 
-            <form onSubmit={changePassword}>
-                <h1>Change Password</h1>
-                <input value={new_password} onChange={(e) => setNewPass(e.target.value)} type="text" placeholder="New Password" required/>
-                <button type="submit">Confirm change</button>
-            </form>
+            </div>
 
-            <button onClick={deleteAccount}> delete account </button>
+
+            <section className="p-8">
+                <QuestionTable />
+            </section>
+
+            <div>
+            </div>
         </div>
     );
-
-    // const [authenticated, setAuthenticated] = useState(null);
-    // useEffect(() => {
-    //     const loggedInUser = localStorage.getItem("authenticated");
-    //     if (loggedInUser) {
-    //         setAuthenticated(loggedInUser);
-    //     }
-    // }, []);
-    //
-    // if (!authenticated) {
-    //     return <Navigate replace to="/login" />;
-    // } else {
-    //     return (
-    //         <div>
-    //             <p>Welcome to your Dashboard</p>
-    //         </div>
-    //     );
-    // }
 
 };
 
