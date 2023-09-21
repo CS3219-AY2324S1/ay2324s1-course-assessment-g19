855056e29b3a943fd3c86c20f59d4dfba3556567
garethkoh <e0533656@u.nus.edu>
2023-09-22 02:49:17 +0800

    Update css and implement password hashing

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -2,54 +2,89 @@ import {useState} from "react";
 import * as React from "react";
 import api from './reactapi';
 import {useNavigate} from "react-router-dom";
-
+import bcrypt from 'bcryptjs';
 
 export const Login = (props) => {
     const [username, setUser] = useState('');
     const [password, setPass] = useState('');
+    const [showFailureMessage, setShowFailureMessage] = useState(false);
 
     const fetchUser = async (username) => {
         try {
             return await api.get('/users/' + username);
         } catch (error) {
-            console.log("no such user");
+            console.log("no such user", error);
+            throw error;
         }
     }
 
     const navigate = useNavigate();
 
+    const showFailure = () => {
+        setShowFailureMessage(true);
+
+        // Hide the failure message after 3 seconds (adjust the duration as needed)
+        setTimeout(() => {
+            setShowFailureMessage(false);
+        }, 3000);
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const response = await fetchUser(username);
+        let response;
+        try {
+            response = await fetchUser(username);
+        } catch (error) {
+            if (error.response && error.response.status === 404) {
+                showFailure();
+                console.log(error.response.data.error);
+            } else {
+                console.error("An error occurred:", error.message);
+            }
+        }
+
         const jsonResponse = JSON.stringify(response)
         const jsonObject = JSON.parse(jsonResponse);
         console.log(jsonObject.data);
-        const user = jsonObject.data.username;
-        const pw = jsonObject.data.password;
-        if (username == user && password == pw) {
-            console.log("logged in")
-            navigate(`userDashboard/${username}`);
-        } else {
-            console.log("incorrect username or password");
-        }
+        const hashed_pw = jsonObject.data.password;
+        bcrypt.compare(password, hashed_pw, async (err, result) => {
+            if (err) {
+                // Handle the error (e.g., log or send an error response)
+                console.error('Password comparison error:', err);
+            } else if (result) {
+                // Passwords match, authentication successful
+                console.log('Authentication successful');
+                navigate(`userDashboard/${username}`);
+            } else {
+                // Passwords do not match, authentication failed
+                showFailure();
+                console.log('Authentication failed');
+            }
+        });
     }
 
     return (
         <div className="flex flex-col justify-center items-center">
+
             <form onSubmit={handleSubmit}>
-                <h1 className="text-purple">Login</h1>
-                <div>
-                    <input className="py-1" value={username} onChange={(e) => setUser(e.target.value)} type="text" placeholder="Username" required/>
+                <h1 className="text-purple text-center font-bold mb-4">Login</h1>
+                <div className="border rounded mb-2">
+                    <input className="py-1 px-2 w-full" value={username} onChange={(e) => setUser(e.target.value)} type="text" placeholder="Username" required/>
                 </div>
-                <div>
-                    <input className="py-1" value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required/>
+                <div className="border rounded mb-2">
+                    <input className="py-1 px-2 w-full" value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required/>
                 </div>
-                <div>
-                    <button className="bg-blue-400 rounded text-white hover:bg-blue-600" type="submit">Login</button>
+                <div className="flex justify-center">
+                    <button className="bg-blue-400 rounded text-white hover:bg-blue-600 py-1 px-2 w-full" type="submit">Login</button>
                 </div>
 
             </form>
-            <button className="bg-red-400 rounded" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register</button>
+            <p className="mt-2">Don't have an account? <span className="text-blue-400 hover:text-blue-600 cursor-pointer" onClick={() => props.onFormSwitch('register')}>Register</span></p>
+            {showFailureMessage && (
+                <div className="bg-red-200 text-red-700 py-2 px-4 rounded-md mb-4">
+                    Wrong username or password.
+                </div>
+            )}
         </div>
     )
 }
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -1,6 +1,8 @@
 import {useState} from "react";
 import * as React from "react";
 import api from './reactapi';
+import bcrypt from 'bcryptjs';
+
 
 export const Register = (props) => {
     const [username, setUser] = useState('');
@@ -10,9 +12,13 @@ export const Register = (props) => {
 
     const handleSubmit = async (e) => {
         if (password === confirmPass) {
+
+            const saltRounds = 10; // Number of salt rounds
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+
             const formData = {
                 username: username,
-                password: password,
+                password: hashedPassword,
             };
             console.log(formData);
             await api.post('/users/', formData);
@@ -28,21 +34,21 @@ export const Register = (props) => {
     return (
         <div className="flex flex-col justify-center items-center">
             <form onSubmit={handleSubmit}>
-                <h1>Register</h1>
-                <div>
-                    <input value={username} onChange={(e) => setUser(e.target.value)} type="text" placeholder="Username" required/>
+                <h1 className="text-purple text-center font-bold mb-4">Register</h1>
+                <div className="border rounded mb-2">
+                    <input className= "py-1 px-2 w-full" value={username} onChange={(e) => setUser(e.target.value)} type="text" placeholder="Username" required/>
                 </div>
-                <div>
-                    <input value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required/>
+                <div className="border rounded mb-2">
+                    <input className= "py-1 px-2 w-full" value={password} onChange={(e) => setPass(e.target.value)} type="password" placeholder="Password" required/>
                 </div>
-                <div>
-                    <input value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} type="password" placeholder="Confirm Password" required/>
+                <div className="border rounded mb-2">
+                    <input className= "py-1 px-2 w-full" value={confirmPass} onChange={(e) => setConfirmPass(e.target.value)} type="password" placeholder="Confirm Password" required/>
                 </div>
-                <div>
-                    <button type="submit">Register</button>
+                <div className="flex justify-center">
+                    <button className="bg-blue-400 rounded text-white hover:bg-blue-600 py-1 px-2 w-full" type="submit">Register</button>
                 </div>
             </form>
-            <button onClick={() => props.onFormSwitch('login')}>Already have an account? Sign in</button>
+            <p className="mt-2">Already have an account? <span className="text-blue-400 hover:text-blue-600 cursor-pointer" onClick={() => props.onFormSwitch('login')}>Sign in</span></p>
         </div>
     )
 }
