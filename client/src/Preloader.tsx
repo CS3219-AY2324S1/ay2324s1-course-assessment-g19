c56ea5742643eff68544f175fddcf16e76510842
tituswe <tituslowesg@gmail.com>
2023-09-28 16:47:32 +0700

    Fix auth bug

--- client/src/Preloader.tsx
+++ client/src/Preloader.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { fetchQuestions } from './features/questions/questionsSlice';
+import { checkAuthStatus } from './features/user/authSlice';
 import { store } from './store';
 
 const Preloader = () => {
   useEffect(() => {
+    store.dispatch(checkAuthStatus());
     store.dispatch(fetchQuestions());
   }, [store]);
 
