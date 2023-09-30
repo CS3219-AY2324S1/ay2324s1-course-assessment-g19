f5ae197393eca306ddb02d582e42c0d2427aa96a
tituswe <tituslowesg@gmail.com>
2023-09-30 23:36:02 +0800

    Fix sizing playbox

--- client/src/components/Playbox/ConfigSelect.tsx
+++ client/src/components/Playbox/ConfigSelect.tsx
@@ -1,5 +1,5 @@
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
-import { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { toCamelCase } from '../../utils/string';
 
 interface ConfigSelectProps {
@@ -18,14 +18,31 @@ const ConfigSelect: React.FC<ConfigSelectProps> = ({
   icon
 }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const handleClick = useCallback((e: any) => {
     callback(e);
     setIsOpen(false);
   }, []);
 
+  const handleClickOutside = (event: globalThis.MouseEvent) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target as Node)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4" ref={dropdownRef}>
       <div
         onClick={() => setIsOpen(!isOpen)}
         className="flex flex-row items-center justify-between gap-4 text-center w-64 p-4 rounded-lg text-gray-100 bg-gray-500 shadow-inner cursor-pointer transition hover:opacity-90"
@@ -37,12 +54,12 @@ const ConfigSelect: React.FC<ConfigSelectProps> = ({
         <ChevronDownIcon className="h-4 w-4 inline-block" />
       </div>
       {isOpen && (
-        <div className="flex flex-col rounded-lg">
+        <div className="flex flex-col rounded-lg max-h-48 overflow-auto">
           {options.map((item, index) => (
             <div
               key={index}
               onClick={() => handleClick(item)}
-              className={`gap-4 w-64 p-4  text-gray-100 bg-gray-500 cursor-pointer transition hover:opacity-90 ${
+              className={`gap-4 w-64 p-4 text-gray-100 bg-gray-500 cursor-pointer transition hover:opacity-90 ${
                 index === 0 && 'rounded-t-lg'
               } ${index === options.length - 1 && 'rounded-b-lg'}`}
             >
