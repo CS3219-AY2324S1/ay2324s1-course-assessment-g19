9a1c65bbec8e77634648638215d04b48aaf81e34
vanessaxuuan <64406694+vanessaxuuan@users.noreply.github.com>
2023-10-20 19:55:03 +0800

    edit ui

--- client/src/components/Questions/Read/QuestionTag.tsx
+++ client/src/components/Questions/Read/QuestionTag.tsx
@@ -4,7 +4,7 @@ interface QuestionTagProps {
 
 const QuestionTag: React.FC<QuestionTagProps> = ({ tag }) => {
   return (
-    <div className="text-xs font-semibold text-rose-500 rounded-md px-2 py-1 bg-gray-100 shadow-md">
+    <div className="text-xs font-semibold text-rose-500 rounded-md px-2 py-1 bg-gray-100 shadow-md w-full">
       {tag}
     </div>
   );
