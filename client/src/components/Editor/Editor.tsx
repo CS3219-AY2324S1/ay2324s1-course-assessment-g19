637c420945600c5b9fa2c71600cd870312b93bb9
tricixg <64681919+tricixg@users.noreply.github.com>
2023-10-31 18:15:45 +0800

    Working Code Execution
    Code is able to execute in Java language

--- client/src/components/Editor/Editor.tsx
+++ client/src/components/Editor/Editor.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import AceEditor from 'react-ace';
-import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
 import PlayerCard from './PlayerCard';
 
 const Editor = () => {
-  const [code, setCode] = React.useState('');
+  const [code, setCode] = React.useState('public class Main {\n  public static void main(String[] args) {\n    System.out.println("Hello, World!");\n  }\n}');
+  const [output, setOutput] = React.useState('');
+  const [isRunning, setIsRunning] = React.useState(false);
 
   const handleCodeChange = (newCode: string) => {
     setCode(newCode);
@@ -14,12 +16,64 @@ const Editor = () => {
 
   const handleRunCode = async () => {
     try {
-      const response = await axios.post('http://localhost:3001/run-code', { code });
-      console.log('Code execution result:', response.data);
+      console.log("handleRunCode called");
+      setIsRunning(true);
+
+      const response = await axios.post(`http://localhost:8000/run-code`, {
+        source_code: code,
+        language_id: 62, // Update this to the correct language_id for Java
+      });
+      console.log(code);
+  
+      const token = response.data.token;
+  
+      if (token) {
+        console.log('Submission token:', token);
+  
+        const maxAttempts = 10;
+        let attempts = 0;
+        let resultResponse;
+  
+        while (attempts < maxAttempts) {
+          resultResponse = await axios.get(`http://localhost:8000/submission/${token}`);
+          console.log('Submission result:', resultResponse.data);
+  
+          if (resultResponse.data.status_id === 3) { // Assuming status_id 3 means 'Finished'
+            setOutput(resultResponse.data.stdout || resultResponse.data.stderr || 'No output');
+            setIsRunning(false);
+            return;
+          } else if (resultResponse.data.status_id > 3) { // Statuses higher than 3 indicate an error or other non-successful state
+            setOutput('Error executing code');
+            setIsRunning(false);
+            return;
+          }
+  
+          attempts++;
+          await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 2 seconds before checking again
+        }
+  
+        setOutput('Execution timed out');
+        setIsRunning(false);
+
+      } else {
+        console.error('No token received from the server');
+        setOutput('Error executing code: No token received');
+        setIsRunning(false);
+
+      }
     } catch (error) {
-      console.error('Error executing code:', error);
+      if (axios.isAxiosError(error)) {
+        console.error('Error executing code:', error.response?.data || error.message);
+      } else {
+        console.error('An unknown error occurred');
+      }
+      setOutput('Error executing code');
+      setIsRunning(false);
+
     }
   };
+  
+  
 
   return (
     <div className="flex flex-col py-8 pl-8 w-full h-full">
@@ -27,21 +81,27 @@ const Editor = () => {
 
       <div className="border-4 border-dashed border-gray-800 flex flex-grow rounded-lg my-4">
         <AceEditor
-          mode="javascript"
+          mode="java"
           theme="github"
           onChange={handleCodeChange}
           name="code_editor"
           value={code}
           editorProps={{ $blockScrolling: true }}
-          style={{ width: '100%', height: '100%' }}
+          style={{ width: '100%', height: '300px' }} // Set a fixed height
         />
       </div>
-      <button 
-        onClick={handleRunCode} 
-        className="my-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Run Code
-      </button>
+      <button
+      onClick={handleRunCode}
+      className="my-4 px-4 py-2 bg-blue-500 text-white rounded"
+      disabled={isRunning}
+    >
+      {isRunning ? "Running..." : "Run Code"}
+    </button>
+
+      <div className="border-2 border-dashed border-gray-800 rounded-lg my-4 p-2">
+        <p>Output:</p>
+        <pre>{output}</pre>
+      </div>
 
       <PlayerCard self />
     </div>
--- question-api/src/index.ts
+++ question-api/src/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from 'express';
-import questionRoutes from './routes/questions';
+
+const axios = require('axios');
+const cors = require('cors');
 
 require('dotenv').config();
 require('./mongodb/db');
@@ -7,13 +9,90 @@ require('./mongodb/db');
 const app: Express = express();
 const port = process.env.SERVER_PORT;
 
+const corsOptions = {
+  origin: 'http://localhost', // Replace with your frontend's origin
+  credentials: true, 
+  methods: ['POST', 'GET'], // Allow both POST and GET
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('from Question API!');
 });
 
-app.use(express.json());
+// Assuming questionRoutes is correctly set up
+import questionRoutes from './routes/questions';
 app.use('/questions', questionRoutes);
 
+app.post('/run-code', async (req, res) => {
+  try {
+    const { source_code, language_id } = req.body;
+
+    const options = {
+      method: 'POST',
+      url: 'https://judge0-ce.p.rapidapi.com/submissions',
+      params: { base64_encoded: 'true', fields: '*' },
+      headers: {
+        'Content-Type': 'application/json',
+        'X-RapidAPI-Key': process.env.JUDGE0_API_KEY!,
+        'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com',
+      },
+      data: {
+        language_id,
+        source_code: Buffer.from(source_code).toString('base64'),
+      },
+    };
+
+    const response = await axios(options);
+    // If the request was successful, send the response back to the client
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error while running code:', error);
+
+    // if (axios.isAxiosError(error) && error.response) {
+    //   // If the error comes from Axios, and there is a response available, send it back to the client
+    //   res.status(error.response.status).json(error.response.data);
+    // } else {
+    //   // For other types of errors, send a generic error message
+    //   res.status(500).json({ error: 'Internal server error' });
+    // }
+  }
+});
+
+app.get('/submission/:token', async (req, res) => {
+  try {
+    const token = req.params.token;
+    const options = {
+      method: 'GET',
+      url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+      params: {
+        base64_encoded: 'false',
+        fields: 'stdout,stderr,status_id,language_id,status'
+      },
+      headers: {
+        'X-RapidAPI-Key': process.env.JUDGE0_API_KEY,
+        'X-RapidAPI-Host': 'judge0-ce.p.rapidapi.com',
+      },
+    };
+
+    const response = await axios.request(options);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching submission result:', error);
+    // if (axios.isAxiosError(error) && error.response) {
+    //   res.status(error.response.status).send(error.response.data);
+    // } else {
+    //   res.status(500).send('Internal Server Error');
+    // }
+  }
+});
+
+
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is alive at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
